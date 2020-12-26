1e12ba1188fc8111a42c98d5e8cd8a30e23ca672
ally-sorloth <allysorloth@gmail.com>
2020-12-26 20:55:40 +0300

    create base and other html files

--- src/fscohort/views.py
+++ src/fscohort/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import StudentForm
+from .models import Student
+
 
 def home_view(request):
     # print(request.GET.get("q"))
@@ -8,17 +10,32 @@ def home_view(request):
     # print(request.user)
     # print(request.path)
     # print(request.method)
-    form = StudentForm()
-    my_context = {
-        'title' : 'clarusway',
-        'dict_1': {'django': 'best framework'},
-        'my_list': [2, 3, 4, 5],
-        'cat': 'maviş',
-        'from': form
-    }
-    return render (request, "fscohort/home.html", my_context)
+    # form = StudentForm()
+    # my_context = {
+    #     'title': '<b>clarusway</b>',
+    #     'dict_1': {'djang': 'best framework'},
+    #     'my_list': [2, 3, 4, 5],
+    #     'cat': 'maviş',
+    #     'student': form
+    # }
+    return render(request, "fscohort/home.html")
 
 # Create your views here.
 
-def about_view(request):
-    return HttpResponse("About Page")
+
+def student_list(request):
+    students = Student.objects.all()
+    context = {
+        'students': students
+    }
+    return render(request, "fscohort/student_list.html", context)
+
+def student_add(request):
+    form = StudentForm()
+    if request.method == "POST"
+    
+    
+    context = {
+        'form': form
+    }
+    return render(request, "fscohort/student_add.html", context)
